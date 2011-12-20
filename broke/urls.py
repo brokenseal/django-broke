7c7c401c1a97a1624039016c0c631240a6fabbc1
Davide Callegari <davide@brokenseal.it>
2011-12-20 12:09:28 +0100

    Changed the url regexp

--- broke/urls.py
+++ broke/urls.py
@@ -5,12 +5,12 @@ urlpatterns= patterns('broke.views',
     url(r'^$', 'root', {}, 'broke-root'),
 
     # data urls
-    url(r'^data/(?P<app_label>\w)/$', 'data', {}, 'broke-data-app'),
-    url(r'^data/(?P<app_label>\w)/(?P<model_name>\w)/$', 'data', {}, 'broke-data-model'),
-    url(r'^data/(?P<app_label>\w)/(?P<model_name>\w)/(?P<pk>\w)/$', 'data', {}, 'broke-data-object'),
+    url(r'^data/(?P<app_label>[a-zA-Z0-9_\-]+)/$', 'data', {}, 'broke-data-app'),
+    url(r'^data/(?P<app_label>[a-zA-Z0-9_\-]+)/(?P<model_name>[a-zA-Z0-9_\-]+)/$', 'data', {}, 'broke-data-model'),
+    url(r'^data/(?P<app_label>[a-zA-Z0-9_\-]+)/(?P<model_name>[a-zA-Z0-9_\-]+)/(?P<pk>[0-9])/$', 'data', {}, 'broke-data-object'),
 
     # meta data urls
     url(r'^meta_data/$', 'meta_data', {}, 'broke-meta-data'),
-    url(r'^meta_data/(?P<app_label>\w)/$', 'meta_data', {}, 'broke-meta-app'),
-    url(r'^meta_data/(?P<app_label>\w)/(?P<model_name>\w)/$', 'meta_data', {}, 'broke-meta-model'),
+    url(r'^meta_data/(?P<app_label>[a-zA-Z0-9_\-]+)/$', 'meta_data', {}, 'broke-meta-app'),
+    url(r'^meta_data/(?P<app_label>[a-zA-Z0-9_\-]+)/(?P<model_name>[a-zA-Z0-9_\-]+)/$', 'meta_data', {}, 'broke-meta-model'),
 )
